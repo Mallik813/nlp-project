b3b9439a90d365bf5f3ba1c1d0d9fcf8918b7278
Mallik813 <mallik813@gmail.com>
2022-04-20 21:29:59 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import react, { useState } from 'react'
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedFile, setSelectedFile] = useState();
+    const [isFilePicked, setIsFilePicked] = useState(false);
+
+    const changeHandler = (event) => {
+        setSelectedFile(event.target.files[0]);
+        setIsFilePicked(true);
+    };
+
+    const handleClick = async () => {
+        const req = await axios.get('http://127.0.0.1:5000/');
+        console.log(req)
+    }
+
+    const handleSubmission = async () => {
+        console.log('idhar hoon mai', selectedFile)
+
+        const req = await axios.post('http://127.0.0.1:5000/', { selectedFile });
+        console.log(req)
+
+    }
+    return (
+        <>
+            <div className='App' >
+                <h1> RESUME PARSER </h1>
+                <h2>Upload Your resume - we will take care of the rest ;)</h2>
+
+            </div >
+            <div className='upload'>
+
+                <div className='card'>
+                    <input type="file" name="file" onChange={changeHandler} />
+                    <div>
+                        <button onClick={handleSubmission} > Submit </button>
+                    </div>
+                </div>
+
+                {/* <table>
+                    <tr>
+                        <th> Field </th>
+                        <th> Data </th>
+                    </tr>
+                </table> */}
+            </div>
+            <div className='data'>
+
+            </div>
+        </>
+    );
 }
 
 export default App;
